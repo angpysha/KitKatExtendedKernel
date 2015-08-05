4e36eaa36e39cffc528918f6f44ebd6d494bf1ac
angpysha <angpysha@outlook.com>
2015-08-05 08:22:11 +0000

    Disabled SELinux

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Tue Aug 4 16:37:46 UTC 2015"
+#define UTS_VERSION "#3 SMP PREEMPT Wed Aug 5 08:08:53 UTC 2015"
 #define LINUX_COMPILE_BY "angpysha"
 #define LINUX_COMPILE_HOST "angpysha"
-#define LINUX_COMPILER "gcc version 4.8 (GCC) "
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
