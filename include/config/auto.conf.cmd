d1560aea51a15dc8e408b7a7dc33256a3ebbd79c
angpysha <angpysha@outlook.com>
2015-08-06 17:29:31 +0000

    Init f2fs support

--- include/config/auto.conf.cmd
+++ include/config/auto.conf.cmd
@@ -84,6 +84,7 @@ deps_config := \
 	fs/reiserfs/Kconfig \
 	fs/jbd2/Kconfig \
 	fs/jbd/Kconfig \
+	fs/f2fs/Kconfig \
 	fs/ext4/Kconfig \
 	fs/ext3/Kconfig \
 	fs/ext2/Kconfig \
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Wed Aug 5 13:23:04 UTC 2015"
+#define UTS_VERSION "#9 SMP PREEMPT Thu Aug 6 11:35:01 UTC 2015"
 #define LINUX_COMPILE_BY "angpysha"
 #define LINUX_COMPILE_HOST "angpysha"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
