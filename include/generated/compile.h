1f2a95958440724296d856487bf57d8f6f17ae63
angpysha <angpysha@outlook.com>
2015-08-08 17:24:13 +0000

    Include zen-iosched

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 10 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 SMP PREEMPT Thu Aug 6 11:35:01 UTC 2015"
+#define UTS_VERSION "#10 SMP PREEMPT Sat Aug 8 11:39:04 UTC 2015"
 #define LINUX_COMPILE_BY "angpysha"
 #define LINUX_COMPILE_HOST "angpysha"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
