16c3c4b601990ac36fbb07baa9845d83679f1336
angpysha <angpysha@outlook.com>
2015-08-12 08:50:10 +0000

    Fix LZ4, Modified freq table

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 31 */
+/* This file is auto generated, version 33 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#31 SMP PREEMPT Tue Aug 11 19:32:22 UTC 2015"
+#define UTS_VERSION "#33 SMP PREEMPT Wed Aug 12 08:41:06 UTC 2015"
 #define LINUX_COMPILE_BY "angpysha"
 #define LINUX_COMPILE_HOST "angpysha"
 #define LINUX_COMPILER "gcc version 4.9.3 20141119 (release) [ARM/embedded-4_9-branch revision 218278] (GNU Tools for ARM Embedded Processors) "
