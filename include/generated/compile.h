94d1e7c142e3ebda4df15c65166e40184f91885c
angpysha <angpysha@outlook.com>
2015-08-14 12:32:01 +0000

    Tried to add kexec-hardboot, but phone didn't boot up

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 49 */
+/* This file is auto generated, version 59 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#49 SMP PREEMPT Thu Aug 13 16:08:53 UTC 2015"
+#define UTS_VERSION "#59 SMP PREEMPT Fri Aug 14 12:17:31 UTC 2015"
 #define LINUX_COMPILE_BY "angpysha"
 #define LINUX_COMPILE_HOST "angpysha"
 #define LINUX_COMPILER "gcc version 5.1.1 20150715 (UBERTC-5.1.1) "
